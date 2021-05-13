75eede655269617e56501edb76013748a0d3e90f
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-14 01:38:01 +0530

    fix bug error response function

--- src/utils/errorResponse.js
+++ src/utils/errorResponse.js
@@ -1,6 +1,8 @@
-module.exports.errorResponse = function (res, error, statusCode, message) {
+module.exports.errorResponse = function (res, e, statusCode, message) {
     if (!statusCode) {
         statusCode = e.statusCode;
     }
-    return res.status(statusCode).json({ errors: {body:[message, e.message]} });
+    return res
+        .status(statusCode)
+        .json({ errors: { body: [message, e.message] } });
 };
