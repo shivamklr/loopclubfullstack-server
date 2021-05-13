3d854ed1359054b5bc8bd96f12aaf489634f3bb7
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-14 01:56:13 +0530

    fix memory leak in users object

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,10 +5,10 @@ const { errorResponse } = require("../utils/errorResponse");
 // GET /api/users/
 router.get("/", async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await User.find().lean();
         return res.status(200).json({ data: { users } });
     } catch (error) {
-        return errorResponse(res, error, 422, "GET request FAILED");
+        return errorResponse(res, error, 422, "GET REQUEST FAILED");
     }
 });
 module.exports = router;
